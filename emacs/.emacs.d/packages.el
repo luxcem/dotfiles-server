e8ab08bee687f6223435b9c4fbe07acfa3c73856
luxcem <a@luxcem.fr>
2019-02-08 11:33:20 +0100

    Fixes emacs packages.

--- emacs/.emacs.d/packages.el
+++ emacs/.emacs.d/packages.el
@@ -1,7 +1,19 @@
 (require 'package)
+
+(setq package-list '(company company-quickhelp
+                             fill-column-indicator multiple-cursors
+                             haml-mode labburn-theme))
+                                        ; ("marmalade" . "https://marmalade-repo.org/packages/")
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/" )))
+
 (package-initialize)
 
+                                        ; Fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
